5e4fca30ed5a59cfe5a0f5cab82176735524945a
Yuna <52529145+YunaBlood@users.noreply.github.com>
2025-09-29 02:48:49 +0200

    Create file for substrings made the dictionary and downcase word
    Joined the dictionary array and split it by using chars to get an array of chars

---
+++ BasicRubyProject/substrings.rb
@@ -0,0 +1,25 @@
+#Define array of substring as dictionary to check
+#Define method substring with word and dictionary as argument
+#Make the word cases insensitive
+#Pass the dictionary into chars to make it an array of character
+#
+#make a select to check if word is included inside dictionary
+#if yes create an array with that dictionary name and with how many time you find it inside.
+#display the result key(included inside directory) and value(count)
+
+#Create a substring that take a word and check if that word character is inside the dictionary and if yes give that string the count of it found in the word
+#
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word, dictionary)
+
+word = word.downcase
+
+dictionary = dictionary.join.chars
+
+
+
+end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
