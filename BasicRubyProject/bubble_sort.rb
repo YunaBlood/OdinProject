63de97a7cf55493073fecde7b7cdecfd1a78ee0e
Yuna <52529145+YunaBlood@users.noreply.github.com>
2025-10-07 00:53:49 +0200

    Basic pseudocode

---
+++ BasicRubyProject/bubble_sort.rb
@@ -0,0 +1,12 @@
+#bubble_sort(array)
+#For each number:
+# For each second number:
+#   If second number < number
+#     Swap the value between each number until array is sorted
+#   else
+#     Ignore
+# Display result
+
+
+
+
