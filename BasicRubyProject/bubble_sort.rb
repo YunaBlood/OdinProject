ecdf067947d4450076ce7022bf0438f96da43afa
Yuna <52529145+YunaBlood@users.noreply.github.com>
2025-10-07 07:41:27 +0200

    Reworked bubble_sort pseudocode

--- BasicRubyProject/bubble_sort.rb
+++ BasicRubyProject/bubble_sort.rb
@@ -1,12 +1,13 @@
 #bubble_sort(array)
-#For each number:
-# For each second number:
-#   If second number < number
-#     Swap the value between each number until array is sorted
-#   else
-#     Ignore
-# Display result
-
+#initialized a sorted variable to false
+#Make a loop that loop until sorted is equal to true which mean all element are sorted
+#update variable sorted to true which will pass after the loop is started and stop the loop at the beginning without needing to redo the loop
+#do a loop that compare between the two side by side number and do that for each of the number of the list/array
+#if the number of the left is lesser than the number on the right swap them otherwise don't
+#Swap the value between each number until array is sorted
+#Update sorted to false because if we entered the condition that mean not all number are sorted and we need to redo a loop to sort again
+#return result
+#Additional add number of iteration and edge cases
 
 
 
